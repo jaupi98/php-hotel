64446d692d3555a47e841da3e9dd7327d777a6d7
jaupi98 <alessio.jaupllari@maxwell.mi.it>
2023-07-05 16:24:28 +0200

    esercizzio impostato

--- index.php
+++ index.php
@@ -37,3 +37,20 @@ $hotels = [
     ],
 ];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+       <?php foreach($hotels as $hotel){ ?>
+            <li>
+                <?php echo $hotel['name'].'-'.$hotel['description'].'-'.$hotel['parking'].'-'.$hotel['vote'].'-'.$hotel['distance_to_center']; ?>
+            </li>
+       <?php  } ?>
+    </ul>
+</body>
+</html>
